8e2804a7554f46a3ed3b60c9318c9d2c1d4e6886
tianwangchong <q408596288@vip.qq.com>
2019-10-21 11:15:16 +0800

    增加消息的code码和消息体

--- app/Manager/Sender.php
+++ app/Manager/Sender.php
@@ -19,9 +19,16 @@ class Sender
 {
     // 作为发送room_id的code
     const MSG_ROOM_ID = 1001;
+    const MSG_WAIT_PLAYER = 1002;
+    const MSG_ROOM_START = 1003;
+    const MSG_GAME_INFO = 1004;
 
+    // 消息
     const CODE_MSG = [
-        self::MSG_ROOM_ID => '房间ID',
+        self::MSG_ROOM_ID     => '房间ID',
+        self::MSG_WAIT_PLAYER => '等待其他玩家中……',
+        self::MSG_ROOM_START  => '游戏开始啦~',
+        self::MSG_GAME_INFO   => 'game info'
     ];
 
     /**
